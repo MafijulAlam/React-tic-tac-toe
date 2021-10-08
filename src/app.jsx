85ada5911af9fcda3081602db1d400507598e50f
MafijulAlam <mafijulcse@gmail.com>
2021-10-08 12:24:37 +0600

    initial commit

---
+++ src/app.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css';
+
+import Game from "./components/game";
+
+class App extends React.Component{
+    render(){
+        return(
+            <div className='app'>
+                <h1> Tic Tac Toe</h1>
+                <Game/>
+            </div>
+        )
+    }
+}
+
+export default App;
